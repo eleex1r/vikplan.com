576b4575a994217e2f80ad00ede761f3a53c07c6
Rinat Gilmanov <solo.gm@gmail.com>
2020-06-08 12:00:09 +0300

    изменены коэфициенты для тайных на 2 и 3 уровнях

--- src/store/db/research_multi.js
+++ src/store/db/research_multi.js
@@ -1,37 +1,37 @@
 export default
     {
         "secreto_2": {
-            "time_calc": 3.3,
+            "time_calc": 3.0,
             "gold": 7.62,
             "scroll": 3.3,
             "inf": 3.3
         },
         "secreto_3": {
-            "time_calc": 11.7,
+            "time_calc": 9.0,
             "gold": 45.0,
             "scroll": 11.7,
             "inf": 11.7
         },
         "secretd_2": {
-            "time_calc": 3.3,
+            "time_calc": 3.0,
             "gold": 7.62,
             "scroll": 3.3,
             "inf": 3.3
         },
         "secretd_3": {
-            "time_calc": 11.7,
+            "time_calc": 9.0,
             "gold": 45.0,
             "scroll": 11.7,
             "inf": 11.7
         },
         "secretf_2": {
-            "time_calc": 3.3,
+            "time_calc": 3.0,
             "gold": 7.62,
             "scroll": 3.3,
             "inf": 3.3
         },
         "secretf_3": {
-            "time_calc": 11.7,
+            "time_calc": 9.0,
             "gold": 45.0,
             "scroll": 11.7,
             "inf": 11.7
